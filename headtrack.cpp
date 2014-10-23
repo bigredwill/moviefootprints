3a2b051ef29e93c5bce282e5fb3445adf6fc204e
Will <bigredwill@gmail.com>
2014-10-22 19:49:35 -0700

    init

--- headtrack.cpp
+++ headtrack.cpp
@@ -13,41 +13,34 @@ using namespace cv;
 int main(int argc, char const *argv[])
 {
 
-	// std::string clipname = "kittenmittens.avi";
-	std::string clipname = "tards.mp4";
-
+	std::string clipname = "video.mp4";
 	cout << clipname << endl;
-
-	vector<Mat> images;
 
 	VideoCapture cap(clipname);
 
+    //is video open
 	if(!cap.isOpened()){
             cout << "can't read video: " << clipname << endl;
             return -1;
-        }
-    cout << "video height: " <<cap.get(CV_CAP_PROP_FRAME_WIDTH) << endl;
-	cout << "video width: " << cap.get(CV_CAP_PROP_FRAME_HEIGHT) << endl;
+    }
 
+    cout << "video height: " << cap.get(CV_CAP_PROP_FRAME_WIDTH) << endl;
+	cout << "video width: "  << cap.get(CV_CAP_PROP_FRAME_HEIGHT) << endl;
+
+    //get amount of frames in video
 	int fcount = (int)cap.get(CV_CAP_PROP_FRAME_COUNT);
 	cout << "frame count: " << fcount << endl;
 
-
 	Mat frame;
-    // while(cap.read(frame)){
-    // 	cout << "FUCK"<<frame.size() << endl;
-    // }
-
     for(int i = 0; i < fcount; i++) {
-
+        //read in frame from video
     	cap >> frame;
 
-
-		 if (!frame.empty())
-	    	imshow("kittenmittens",frame);
+        //to avoid 0x0 frame error
+		if (!frame.empty())
+	    	imshow("footprint",frame);
     	
     	char key = (char) waitKey('q');
-
     	if(key == 'q')  break;
     	
     }
